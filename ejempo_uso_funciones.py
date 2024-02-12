0570fc89190c1cb3768750c380fb8b7966faca2a
Adonis <adonis.g.anes@gmail.com>
2024-02-12 17:56:40 +0100

    updated

--- ejempo_uso_funciones.py
+++ ejempo_uso_funciones.py
@@ -3,29 +3,28 @@ from consulta_palabras_relevantes import *
 from selenium import webdriver
 from busqueda_SerApi import consulta_a_links
 
-print('--------------------------------------------------------------------------------')
-print("USO DE LA FUNCIÓN 'aceptar_cookies'")
+
+#USO DE LA FUNCIÓN 'aceptar_cookies'
 driver = webdriver.Chrome()
 url='https://cnnespanol.cnn.com/tag/nayib-bukele/'
 aceptar_cookies(driver=driver, url=url, show_process=True)
 driver.quit()
 
-('--------------------------------------------------------------------------------')
-print("USO DE LA FUNCIONES QUE ENGLOBA LA FUNCIÓN 'consulta_a_palabras_relevantes' ")
+# USO DE LA FUNCIONES QUE ENGLOBA LA FUNCIÓN 'consulta_a_palabras_relevantes'
 consulta_cafeina = 'cuál es el componente de la cafeína que actúa en el cerebro'
-print("--------Función 'consulta_a_links' en ejecución--------")
+
+# 1.- Pasar de una consulta escrita a un conjunto de links
 urls_cafeina = consulta_a_links(consulta=consulta_cafeina)
-print("--------Función 'aceptar_cookies_y_guardar_htmls' en ejecución--------")
+# 2.- Meternos en dichas páginas, aceptar las cookies y descargar el html
 htmls_cafeina = aceptar_cookies_y_guardar_htmls(urls=urls_cafeina)
-print("--------Función 'htmls_a_textos' en ejecución--------")
+# 3.- Obtener el texto de los htmls
 textos_cafeina = htmls_a_textos(htmls=htmls_cafeina)
-print("--------Función 'crear_matriz_tfidf' en ejecución--------")
+# 4.- Crear la matriz tfidf de dichos textos
 matriz_tfidf_cafeina = crear_matriz_tfidf(text_files=textos_cafeina)
 print("--------Función 'top_n_palabras_rel_tdidf' en ejecución--------")
+# 5.- Mostrar en pantalla las n palabras relevantes extraídas por la matriz
 palabras_rel_consulta_cafeina = top_n_palabras_rel_tdidf(tfidf=matriz_tfidf_cafeina)
 print('Palabras relevantes de la consulta: ',palabras_rel_consulta_cafeina)
 
-('--------------------------------------------------------------------------------')
-print("USO DE LA FUNCIÓN 'consulta_a_palabras_relevantes'")
-consulta = 'cuál es el componente de la cafeína que actúa en el cerebro'
-consulta_a_palabras_relevantes(consulta)
+# Uso de la función 'consulta_a_palabras_relevantes'
+consulta_a_palabras_relevantes(consulta=consulta_cafeina)
--- test.py
+++ test.py
@@ -1,59 +1,34 @@
 from selenium import webdriver
+import unittest
 
-from .aceptar_cookies import *
-from consulta_palabras_relevantes import *
+from aceptar_cookies import *
 
-def prueba_txt_botones_cookies_a_lista():
-    salida_esperada = ['ej1', 'ej2', 'ej3', 'ej4']
-    salida_obtenida = txt_botones_cookies_a_lista(file_dir='cookies/prueba_txt_botones_cookies_a_lista.txt')
-    if salida_obtenida == salida_esperada:
-        print("Prueba de la función 'txt_botones_cookies_a_lista' pasada correctamente")
-    else:
-        print("Prueba de la función 'txt_botones_cookies_a_lista' NO pasada correctamente")
+class TestCookieMethods(unittest.TestCase):
 
-def prueba_cargar_ficheros_botones_cookies():
-    salida_esperada = (['didomi-notice-agree-button', 'onetrust-accept-btn-handler', 'ez-accept-all', 'hs-eu-confirmation-button', 'pea_cook_btn', 'CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll'], 
-                       ['cdp-cookies-boton-cerrar', 'accept', 'fc-button fc-cta-consent fc-primary-button', 'btn btn-primary btn-sm', 'shareaholic-cookie-consent-button shareaholic-accept-button', 'mgbutton moove-gdpr-infobar-allow-all gdpr-fbo-0', 'cky-btn cky-btn-accept', 'CybotCookiebotDialogBodyButton'])
-    salida_obtenida = cargar_ficheros_botones_cookies()
-    if salida_obtenida == salida_esperada:
-        print("Prueba de la función 'cargar_ficheros_botones_cookies' pasada correctamente")
-    else:
-        print("Prueba de la función 'cargar_ficheros_botones_cookies' NO pasada correctamente")
+    def test_txt_botones_cookies_a_lista(self):
+        salida_esperada = ['ej1', 'ej2', 'ej3', 'ej4']
+        salida_obtenida = txt_botones_cookies_a_lista(file_dir='cookies/prueba_txt_botones_cookies_a_lista.txt')
+        self.assertEqual(salida_obtenida, salida_esperada)
 
-def prueba_aceptar_cookies_sin_descargar_html():
-    url = 'https://www.sonymusic.es/playlist/2010-hits-decade/'
-    driver = webdriver.Chrome()
-    salida_esperada = 1
-    salida_obtenida = aceptar_cookies(driver=driver, url=url)
-    driver.quit()
-    if salida_obtenida == salida_esperada:
-        print("Prueba de la función 'aceptar_cookies' sin descargar html pasada correctamente")
-    else:
-        print("Prueba de la función 'aceptar_cookies' sin descargar html NO pasada correctamente")
+    def test_cargar_ficheros_botones_cookies(self):
+        salida_esperada = (['didomi-notice-agree-button', 'onetrust-accept-btn-handler', 'ez-accept-all', 'hs-eu-confirmation-button', 'pea_cook_btn', 'CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll'], 
+                        ['cdp-cookies-boton-cerrar', 'accept', 'fc-button fc-cta-consent fc-primary-button', 'btn btn-primary btn-sm', 'shareaholic-cookie-consent-button shareaholic-accept-button', 'mgbutton moove-gdpr-infobar-allow-all gdpr-fbo-0', 'cky-btn cky-btn-accept', 'CybotCookiebotDialogBodyButton'])
+        salida_obtenida = cargar_ficheros_botones_cookies()
+        self.assertEqual(salida_obtenida, salida_esperada)
 
-def prueba_aceptar_cookies_descargando_html():
-    url = 'https://www.sonymusic.es/playlist/2010-hits-decade/'
-    driver = webdriver.Chrome()
-    salida_obtenida = aceptar_cookies(driver=driver, url=url, download_html=True)
-    driver.quit()
-    if salida_obtenida != None:
-        print("Prueba de la función 'aceptar_cookies' descargando html pasada correctamente")
-    else:
-        print("Prueba de la función 'aceptar_cookies' descargando html NO pasada correctamente")
+    def test_aceptar_cookies(self):
+        url = 'https://www.sonymusic.es/playlist/2010-hits-decade/'
+        driver = webdriver.Chrome()
+        salida_esperada = 1
+        salida_obtenida = aceptar_cookies(driver=driver, url=url)
+        driver.quit()
+        self.assertEqual(salida_obtenida, salida_esperada)
 
-def prueba_aceptar_cookies_y_guardar_htmls():
-    urls = ['https://www.eluniverso.com/larevista/2021/01/17/nota/9491971/canciones-que-mas-escuchamos-decada-2010-2020/', 
-            'https://www.sonymusic.es/playlist/2010-hits-decade/', 
-            'https://neliosoftware.com/es/blog/como-crear-un-aviso-de-cookies-con-nelio-popups/']
-    salida_obtenida = aceptar_cookies_y_guardar_htmls(urls=urls, show_process=False)
-    if len(salida_obtenida) == 3:
-        print("Prueba de la función 'aceptar_cookies_y_guardar_htmls'  pasada correctamente")
-    else:
-        print("Prueba de la función 'aceptar_cookies_y_guardar_htmls' NO pasada correctamente")
-        
+    def test_aceptar_cookies_y_guardar_htmls(self):
+        urls = ['https://www.eluniverso.com/larevista/2021/01/17/nota/9491971/canciones-que-mas-escuchamos-decada-2010-2020/', 
+                'https://www.sonymusic.es/playlist/2010-hits-decade/']
+        salida_obtenida = aceptar_cookies_y_guardar_htmls(urls=urls, show_process=False)
+        self.assertEqual(len(salida_obtenida), len(urls))  
 
-prueba_txt_botones_cookies_a_lista()
-prueba_cargar_ficheros_botones_cookies()
-#prueba_aceptar_cookies_sin_descargar_html()
-#prueba_aceptar_cookies_descargando_html()
-#prueba_aceptar_cookies_y_guardar_htmls()
+if __name__ == '__main__':
+    unittest.main()
