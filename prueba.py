1a8363e92445edce547823650a762c9c71fb44cb
Adonis <adonis.g.anes@gmail.com>
2024-02-20 16:42:23 +0100

    updated

--- prueba.py
+++ prueba.py
@@ -2,7 +2,7 @@ from functionsAcceptCookies import *
 from functionsReadWriteFiles import *
 import time
 
-id_consulta = 'rodilla'
+id_consulta = 'rusiaHistoria'
 urls = cargar_urls(id_consulta=id_consulta)
 inicio = time.time()
 htmls = aceptar_cookies_y_extraer_htmls_paralelizado(urls=urls)
